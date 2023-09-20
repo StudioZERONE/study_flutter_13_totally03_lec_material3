f102078dad6910905f4a74c50989de7e35ba3907
ZERONE <zerone012@gmail.com>
2023-09-20 09:46:03 +0900

    UI 공부 중. Material 3 디자인 적용

--- lib/main.dart
+++ lib/main.dart
@@ -28,15 +28,90 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> friendList = [
+    'Andrew',
+    'Brian',
+    'Catherine',
+    'Daniel',
+    'John',
+    'Raul',
+    'Wilson',
+    'Zerone',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('')),
-      body: const Center(
-        child: Column(
-          children: [],
+      appBar: AppBar(
+        title: const Text('Material 3 Design'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: friendList.length,
+          itemBuilder: (context, index) {
+            return cardContainer(name: friendList[index], number: index + 1);
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showFloatingDialog();
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget cardContainer({String name = '', int number = 0}) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: ListTile(
+        title: Container(
+          width: MediaQuery.of(context).size.width,
+          padding: const EdgeInsets.all(12),
+          child: Text(
+            '$number. $name',
+            style: const TextStyle(fontSize: 20),
+          ),
+        ),
+        subtitle: Row(
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('Details'),
+            ),
+            FilledButton.tonal(
+              onPressed: () {},
+              child: const Text('Follow'),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  void showFloatingDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(child: Text('Do you want to add a friend?')),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('No'),
+            ),
+            FilledButton(
+              onPressed: () {},
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
